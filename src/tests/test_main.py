43a1432471139033ea8be530300112e7ecec35fa
Francisco Dueñas <frsduenasca@unal.edu.co>
2023-03-18 12:44:16 -0500

    :zap: [UPDATE] New tests!

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -3,14 +3,15 @@ from src.main import finds_pairs
 
 
 def test_finds_pairs():
-    assert sorted(finds_pairs([0, 1, 2, 3], 3)) == [(0, 3), (1, 2)]
+    assert finds_pairs([0, 1, 2, 3], 3) == {(0, 3), (1, 2)}
 
 
 @pytest.mark.parametrize(
     "numbers_list, number_sum, expected_pairs",
     [
-        ([0, 1, 2, 3], 3, [(0, 3), (1, 2)])
+        ([0, 1, 2, 3], 3, {(0, 3), (1, 2)}),
+        ([1, 9, 5, 0, 20, -4, 12, 16, 7], 12, {(0, 12), (5, 7), (-4, 16)})
     ]
 )
-def test_finds_pairs_params(numbers_list: list, number_sum: int, expected_pairs: list):
-    assert sorted(finds_pairs(numbers_list, number_sum)) == expected_pairs
+def test_finds_pairs_params(numbers_list: list, number_sum: int, expected_pairs: set):
+    assert finds_pairs(numbers_list, number_sum) == expected_pairs
