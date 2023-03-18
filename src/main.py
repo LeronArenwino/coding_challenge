a0cacf87cd115509af27f8a8e6f3a2728e6dc04d
Francisco Dueñas <frsduenasca@unal.edu.co>
2023-03-18 12:43:31 -0500

    :zap: [UPDATE] Using sets

--- src/main.py
+++ src/main.py
@@ -6,12 +6,12 @@ def finds_pairs(numbers_list: list, number_sum: int) -> list:
     """Method to find pairs of numbers on array which sum is equals to the given value."""
     unordered_set: set = set()
 
-    pairs: list = []
+    pairs: set = set()
 
     for i in range(len(numbers_list)):
 
         if number_sum - numbers_list[i] in unordered_set:
-            pairs.append(((number_sum - numbers_list[i]), numbers_list[i]))
+            pairs.add(((number_sum - numbers_list[i]), numbers_list[i]))
 
         else:
             unordered_set.add(numbers_list[i])
@@ -20,14 +20,14 @@ def finds_pairs(numbers_list: list, number_sum: int) -> list:
 
 
 def find_pairs_n2(numbers_list: list, number_sum: int) -> list:
-    pairs: list = []
+    pairs: set = set()
 
     for i in range(0, len(numbers_list)):
 
         for j in range(i + 1, len(numbers_list)):
 
             if numbers_list[i] + numbers_list[j] == number_sum:
-                pairs.append((numbers_list[i], numbers_list[j]))
+                pairs.add((numbers_list[i], numbers_list[j]))
 
     return pairs
 
@@ -36,9 +36,9 @@ def compare(list1: list, list2: list) -> bool:
     return sorted(list1) == sorted(list2)
 
 
-# array: list = [1, 9, 5, 0, 20, -4, 12, 16, 7]
+array: list = [1, 9, 5, 0, 20, -4, 12, 16, 7]
 sum_number: int = 12
-array: list = list(set(np.random.randint(-10000, 10000, size=20000)))
+# array: list = list(set(np.random.randint(-10000, 10000, size=20000)))
 
 if __name__ == '__main__':
     # Using hashing
@@ -54,4 +54,4 @@ if __name__ == '__main__':
     print(*pairs2)
 
     # Compare pairs
-    print(compare(pairs1, pairs2))
+    print(pairs1 == pairs2)
