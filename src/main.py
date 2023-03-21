0a232cdf7f5f39ae05428cf20dccded962569829
Francisco Dueñas <frsduenasca@unal.edu.co>
2023-03-20 20:53:40 -0500

    :sparkles: [UPDATE] Added documentation to main.py

--- src/main.py
+++ src/main.py
@@ -3,7 +3,11 @@ import time
 
 
 def finds_pairs(numbers_list: list, number_sum: int) -> list:
-    """Method to find pairs of numbers on array which sum is equals to the given value."""
+    """Method to find pairs of numbers on list which sum is equals to the given value.
+
+    Time complexity: O(n).
+    """
+
     unordered_set: set = set()
 
     pairs: set = set()
@@ -20,6 +24,11 @@ def finds_pairs(numbers_list: list, number_sum: int) -> list:
 
 
 def find_pairs_n2(numbers_list: list, number_sum: int) -> list:
+    """Method to find pairs of numbers on list which sum is equals to the given value.
+
+    Time complexity: O(n^2).
+    """
+
     pairs: set = set()
 
     for i in range(0, len(numbers_list)):
@@ -32,26 +41,29 @@ def find_pairs_n2(numbers_list: list, number_sum: int) -> list:
     return pairs
 
 
-def compare(list1: list, list2: list) -> bool:
-    return sorted(list1) == sorted(list2)
-
-
-array: list = [1, 9, 5, 0, 20, -4, 12, 16, 7]
+# Given value
 sum_number: int = 12
-# array: list = list(set(np.random.randint(-10000, 10000, size=20000)))
+
+# Example list
+#  array: list = [1, 9, 5, 0, 20, -4, 12, 16, 7]
+
+#  Generated list using numpy
+array: list = list(set(np.random.randint(-10000, 10000, size=20000)))
 
 if __name__ == '__main__':
-    # Using hashing
+    # Using hashing. Time complexity: O(n)
+    # Time of execution of finds_pairs method
     start_time = time.time()
     pairs1: list = finds_pairs(array, sum_number)
     print("Seconds: %s" % (time.time() - start_time))
     print(*pairs1)
 
-    # Using for loop
+    # Using for loops. Time complexity: O(n^2)
+    # Time of execution of finds_pairs_n2 method
     start_time = time.time()
     pairs2 = find_pairs_n2(array, sum_number)
     print("Seconds: %s" % (time.time() - start_time))
     print(*pairs2)
 
-    # Compare pairs
+    # Compare find pairs methods
     print(pairs1 == pairs2)
